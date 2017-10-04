459493b1606e811414caa31b811a2bb284ed82fd
vincentlaucsb <vincela9@gmail.com>
2017-10-03 17:33:21 -0700

    Attempted fix of CLI tests not running on Travis

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,17 +5,14 @@ import unittest
 from pgreaper.cli import copy
 
 class CLITest(unittest.TestCase):
-    # Make tests a subclass of CLITest to fix the issue of tests
-    # not running on Travis CI
-    
     def test_read_print_zip(self):
         ''' Test that printing out the contents of a ZIP file works '''
         runner = CliRunner()
         result = runner.invoke(copy, ['--zip', 
             'csv-data/real_data/compressed/2015_StateDepartment.zip'])
             
-        assert result.exit_code == 0
-        assert '[0] 2015_StateDepartment.csv' in result.output
+        self.assertEqual(result.exit_code, 0)
+        self.assertIn('[0] 2015_StateDepartment.csv', result.output)
         
     def test_copy_zip_file_missing(self):
         '''
@@ -25,7 +22,8 @@ class CLITest(unittest.TestCase):
         runner = CliRunner()
         result = runner.invoke(copy, ['--zip', '--csv',
             'csv-data/real_data/compressed/2015_StateDepartment.zip'])
-        assert 'Please specify which file in the ZIP archive to upload' in str(result)
+        self.assertIn('Please specify which file in the ZIP archive to upload',
+            str(result))
     
 if __name__ == '__main__':
     unittest.main()
